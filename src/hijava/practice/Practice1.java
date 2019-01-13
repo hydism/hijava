a80f404a9618562c7abdcb1fd6ad38581d50916e
hydism <hydism@nate.com>
2019-01-13 13:45:02 +0900

    사칙연산, 자료형 변환

---
+++ src/hijava/practice/Practice1.java
@@ -0,0 +1,12 @@
+package hijava.practice;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		double num2 = 2.0;
+		System.out.println((int)(num1 + num2));
+		System.out.println((int)(num1 - num2));
+		System.out.println((int)(num1 * num2));
+		System.out.println((int)(num1 / num2));
+	}
+}
