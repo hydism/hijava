5e293456e7d09c7e3d0e7997464d27c82f2e6a4a
DESKTOP-A97U8UL\hydis <hydism@nate.com>
2019-01-19 23:32:31 +0900

    과제 최종

--- src/hijava/practice/Practice3.java
+++ src/hijava/practice/Practice3.java
@@ -5,6 +5,7 @@ public class Practice3 {
 	public static void main(String[] args) {
 		// 1~100까지 소수들의 합
 		int sum = 0;
+		System.out.print("소수 : ");
 		for(int i = 2; i <= 100; i++) {
 			int cnt = 0;
 			for(int j = 2; j <= i - 1; j++) {
@@ -15,9 +16,12 @@ public class Practice3 {
 				}
 			
 				if(cnt == 0) {
+					
+					System.out.print(i + " ");
 					sum += i;
 				}
 			}
+		System.out.println();
 		System.out.println("1~100까지 소수들의 총 합 : " + sum);
 		}
 	}
